c46a169b39f68173a5ff72557996dca0b5f6b04c
Camilo Antonio Arteta Coronell <carteta@jamar.com>
2024-12-13 00:18:30 -0500

    Para QA

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { productRouter } from './routes/productos.js'
+import cors from 'cors'
 import { Synonyms } from './synonyms/synonyms.js'
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/productos', productRouter)
 
 app.get('/', (req, res) => {
@@ -18,6 +20,6 @@ app.get('/prueba', async (req, res) => {
     res.send(result)
 })
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0',() => {
     console.log('App listening on 3000')
 })
